cf3b5ce657c18b19ca7bc1547dd5ad667a1d2438
kingcharlezz <39387371+kingcharlezz@users.noreply.github.com>
2023-04-01 19:32:32 -0400

    Create polkacracker.py

---
+++ polkacracker.py
@@ -0,0 +1,111 @@
+import sys
+from base64 import b64decode
+import struct
+import scrypt
+from nacl.secret import SecretBox
+import threading
+from threading import Lock
+import cupy as cp
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+SCRYPT_DEFAULT_N = 32768
+SCRYPT_DEFAULT_P = 1
+SCRYPT_DEFAULT_R = 8
+
+password_found = False
+password_found_lock = threading.Lock()
+
+ENCODED = "T/ej+6KLpBVeFNaTxx6IVjNxKK3ToqJxti7R/vrg02sAgAAAAQAAAAgAAACCiUOMV0Wp7JrqGtw+IgebYqOMI8Nc81z97BXeCQ/gcji1YgRZICr6236rMy5OPfa6da/Aq8yR4ef7LJ40CFI4an4bIonqg3a40+KMu9QZ+iHVWf39U/TsI+XubAiLrqRcRIBrAq20CEw4TwRjsLYV41spkKocDB4SscxfpwuopUiKEuQ2+unxabsOC3poe4PH161TitOKiXAlXGrW"
+
+counter_lock = Lock()
+counter = 0
+
+def update_counter():
+    global counter
+    with counter_lock:
+        counter += 1
+    return counter
+
+def try_decrypt(password, salt, nonce, encrypted):
+    global password_found
+    with password_found_lock:
+        if password_found:
+            return None
+
+    key = scrypt.hash(password.strip(), salt, N=SCRYPT_DEFAULT_N, r=SCRYPT_DEFAULT_R, p=SCRYPT_DEFAULT_P, buflen=32)
+    box = SecretBox(key)
+    try:
+        box.decrypt(encrypted, nonce)
+        with password_found_lock:
+            password_found = True
+        return password.strip()
+    except:
+        return None
+
+def process_line(line, salt, nonce, encrypted):
+    password = line.strip()
+    current_line = update_counter()
+    print(f"Checking password at line {current_line}", end="\r")
+    result = try_decrypt(password, salt, nonce, encrypted)
+    if result:
+        print(f"Password found: '{result}'")
+    return result
+
+def read_in_chunks(file, block_size=1024):
+    while True:
+        data = file.readlines(block_size)
+        if not data:
+            break
+        yield data
+
+def main():
+    if len(sys.argv) < 2:
+        print("ERROR: Please specify the dict file within the command line", file=sys.stderr)
+        sys.exit(1)
+
+    try:
+        fp = open(sys.argv[1], 'r', encoding='utf-8',)
+    except:
+        print("ERROR: Could not open dictionary file '%s'" % sys.argv[1], file=sys.stderr)
+        sys.exit(1)
+
+    raw_data = b64decode(ENCODED)
+
+    salt = cp.asarray(raw_data[0:32])
+    scrypt_n = struct.unpack("<I", raw_data[32:36])[0]
+    scrypt_p = struct.unpack("<I", raw_data[36:40])[0]
+    scrypt_r = struct.unpack("<I", raw_data[40:44])[0]
+
+    offset = 32 + (3 * 4)
+    nonce = cp.asarray(raw_data[offset:offset + 24])
+    encrypted = cp.asarray(raw_data[offset + 24:])
+
+    cracked = False
+
+    num_processes = 8  # Change this value to modify the number of processes
+
+    for chunk in read_in_chunks(fp):
+        with cp.cuda.Device(0):
+            with ThreadPoolExecutor(max_workers=num_processes) as executor:
+                futures = [executor.submit(process_line, line, salt, nonce, encrypted) for line in chunk]
+
+                for future in as_completed(futures):
+                    result = future.result()
+                    if result:
+                        cracked = True
+                        break
+
+        if cracked:
+            break
+
+    fp.close()
+
+    if cracked:
+        print('cracked')
+        sys.exit(0)
+    else:
+        print('not cracked')
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
